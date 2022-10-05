7b672ceefa733c8dbe59c979cec9ea609cd7596d
Sebastian Madrid <soygenial56@gmail.com>
2022-10-05 14:46:10 -0500

    login en progreso

---
+++ FrontEnd/src/app/Services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = 'http://127.0.0.1:3000/api'
+  constructor(private http: HttpClient) { }
+
+  login(data) {
+    return this.http.post(`${this.url}/login_empleado`, data)
+  }
+}
